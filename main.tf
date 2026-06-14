11873a354df5ccbe9d360fa5e5ace8aa2ab35916
alejny-sudo <alejandro.nyder27@gmail.com>
2026-06-14 23:03:28 +0200

    Change S3 bucket ACL to private and restrict public access
    Updated S3 bucket ACL settings to enhance security.

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "my-guardly-test-bucket"
-  acl    = "public-read"
+  acl = "private"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
